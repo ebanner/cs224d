74c9361682d0b870a9c61f721de2cd5f03cc06a2
Edward Banner <edward.banner@gmail.com>
2015-12-19 14:01:12 -0600

    Update word vectors gradients

--- assignment2/nerwindow.py
+++ assignment2/nerwindow.py
@@ -17,10 +17,14 @@ def full_report(y_true, y_pred, tagnames):
 def eval_performance(y_true, y_pred, tagnames):
     pre, rec, f1, support = metrics.precision_recall_fscore_support(y_true, y_pred)
     print "=== Performance (omitting 'O' class) ==="
-    print "Mean precision:  %.02f%%" % (100*sum(pre[1:] * support[1:])/sum(support[1:]))
-    print "Mean recall:     %.02f%%" % (100*sum(rec[1:] * support[1:])/sum(support[1:]))
-    print "Mean F1:         %.02f%%" % (100*sum(f1[1:] * support[1:])/sum(support[1:]))
+    mean_precision = (100*sum(pre[1:] * support[1:])/sum(support[1:]))
+    mean_recall = (100*sum(rec[1:] * support[1:])/sum(support[1:]))
+    mean_f1 = (100*sum(f1[1:] * support[1:])/sum(support[1:]))
+    print "Mean precision:  %.02f%%" % mean_precision
+    print "Mean recall:     %.02f%%" % mean_recall
+    print "Mean F1:         %.02f%%" % mean_f1
 
+    return mean_precision, mean_recall, mean_f1
 
 ##
 # Implement this!
@@ -51,13 +55,16 @@ class WindowMLP(NNBase):
         # Set regularization
         self.lreg = float(reg)
         self.alpha = alpha # default training rate
+        self.nclass = dims[2] # number of output classes
+
+        self.D = wv.shape[1]
+        self.windowsize = windowsize
 
         dims[0] = windowsize * wv.shape[1] # input dimension
         param_dims = dict(W=(dims[1], dims[0]),
                           b1=(dims[1],),
                           U=(dims[2], dims[1]),
-                          b2=(dims[2],),
-                          )
+                          b2=(dims[2],))
         param_dims_sparse = dict(L=wv.shape)
 
         # initialize parameters: don't change this line
@@ -65,7 +72,16 @@ class WindowMLP(NNBase):
 
         random.seed(rseed) # be sure to seed this for repeatability!
 
-        # any other initialization you need
+        ##
+        # Now we can access the parameters using
+        # self.params.<name> for normal parameters
+        # self.sparams.<name> for params with sparse gradients
+        # and get access to normal NumPy arrays
+        self.sparams.L = wv.copy() # store own representations
+        self.params.W  = random_weight_matrix(*self.params.W.shape)
+        # self.params.b1 = zeros(*self.params.b1.shape) # done automatically!
+        self.params.U  = random_weight_matrix(*self.params.U.shape)
+        # self.params.b2 = zeros(*self.params.b2.shape) # done automatically!
 
     def _acc_grads(self, window, label):
         """
@@ -84,13 +100,14 @@ class WindowMLP(NNBase):
         
         """
         W, b1, U, b2 = self.params.W, self.params.b1, self.params.U, self.params.b2
-        H, window_size = W.shape
+        H, input_size = W.shape
         C, H = U.shape
-        X = self.sparams.L[window].reshape(window_size, 1)
+        
+        # Convert window indices to input
+        X = self.sparams.L[window].reshape(input_size, 1)
 
         # Forward Pass (predictions)
         z = np.dot(W, X) + b1.reshape(H, 1)
-        #hidden = 1. / (1.+np.exp(-z))
         hidden = np.tanh(z)
         
         # Tanh
@@ -109,11 +126,14 @@ class WindowMLP(NNBase):
         self.grads.U += np.dot(dscores, hidden.T)
         
         dhidden = np.dot(U.T, dscores)
-        #dz = (hidden * (1-hidden)) * dhidden # sigmoid
-        dz = (1 - hidden**2) * dhidden # tanh
+        dz = (1 - hidden**2) * dhidden # tanh derivative
         
         self.grads.b1 += dz.reshape(H)
         self.grads.W += np.dot(dz, X.T)
+
+        # Push input vectors around
+        dX = np.dot(W.T, dz).reshape(self.windowsize, self.D)
+        self.sgrads.L[window] = dX
         
         # Regularization
         loss += 0.5 * self.lreg*(np.sum(W**2) + np.sum(b1**2) + np.sum(U**2) + np.sum(b2**2))
@@ -122,6 +142,29 @@ class WindowMLP(NNBase):
         self.grads.b1 += self.lreg*b1
         self.grads.U  += self.lreg*U
         self.grads.b2 += self.lreg*b2
+        
+    def compute_loss(self, windows, labels):
+        """
+        Compute the loss for a given dataset.
+        windows = same as for predict_proba
+        labels = list of class labels, for each row of windows
+        
+        """
+        if not hasattr(windows[0], "__iter__"):
+            num_examples = 1
+        else:
+            num_examples = len(windows)
+            
+        if not hasattr(labels, "__iter__"):
+            labels = [labels]
+        
+        probs = self.predict_proba(windows)
+        y_hats = probs[range(num_examples), labels]
+        
+        loss = -np.log(y_hats).sum()
+        loss_reg = 0.5 * self.lreg*(np.sum(self.params.W**2) + np.sum(self.params.b1**2) + np.sum(self.params.U**2) + np.sum(self.params.b2**2))
+        
+        return loss + loss_reg
 
     def predict_proba(self, windows):
         """
@@ -137,14 +180,32 @@ class WindowMLP(NNBase):
         # a list-of-lists
         if not hasattr(windows[0], "__iter__"):
             windows = [windows]
+            
+        W, b1, U, b2 = self.params.W, self.params.b1, self.params.U, self.params.b2
+        H, input_size = W.shape
+        C, H = U.shape
 
-        #### YOUR CODE HERE ####
+        def probs_generator():
+            """Yields all the probs"""
 
-        print windows
+            # Convert window indices to matrix of vertical inputs
+            for i, window in enumerate(windows):
+                # Windowize input
+                x = self.sparams.L[window].reshape(input_size, 1)
 
-        #### END YOUR CODE ####
+                # Forward Pass
+                z = np.dot(W, x) + b1.reshape(H, 1)
+                hidden = np.tanh(z)
 
-        return P # rows are output for each input
+                # Scores and softmax
+                scores = np.dot(U, hidden) + b2.reshape(C, 1)
+                probs = softmax(scores)
+
+                yield probs.ravel()
+
+        probs = list(probs_generator())
+
+        return np.array(probs)
 
 
     def predict(self, windows):
@@ -152,37 +213,8 @@ class WindowMLP(NNBase):
         Predict most likely class.
         Returns a list of predicted class indices;
         input is same as to predict_proba
+
         """
-
-        #### YOUR CODE HERE ####
-
-
-        #### END YOUR CODE ####
-        return c # list of predicted classes
-
-
-    def compute_loss(self, windows, labels):
-        """
-        Compute the loss for a given dataset.
-        windows = same as for predict_proba
-        labels = list of class labels, for each row of windows
-        """
-
-        #### YOUR CODE HERE ####
-
-        W, b1, U, b2 = self.params.W, self.params.b1, self.params.U, self.params.b2
-        H, window_size = W.shape
-        C, H = U.shape
-        X = self.sparams.L[windows].reshape(window_size, 1)
-
-        # Forward Pass (predictions)
-        z = np.dot(W, X) + b1.reshape(H, 1)
-        #hidden = 1. / (1.+np.exp(-z)) # sigmoid
-        hidden = np.tanh(z) # sigmoid
+        probs = self.predict_proba(windows)
         
-        # Tanh
-        scores = np.dot(U, hidden) + b2.reshape(C, 1)
-        probs = softmax(scores)
-        y_hat = probs[labels]
-        
-        return -np.log(y_hat)
+        return probs.argmax(axis=1) # list of predicted classes
